1cce2901a6dcb86de38cde793970a08a0fc973b7
Rachel Xiang <rachelxiangg@gmail.com>
2014-05-10 23:55:20 -0400

    what's up with this css yo

---
+++ web/Gruntfile.js
@@ -0,0 +1,23 @@
+/**
+ * Created by rachel on 5/10/14.
+ */
+module.exports = function(grunt) {
+
+
+    grunt.initConfig({
+
+        watch :{
+            scripts :{
+                files : ['js/app.js','css/*.css','index.html'],
+                options : {
+                    livereload : 9090,
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', []);
+};
