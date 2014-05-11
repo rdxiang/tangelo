d8ab5a06a0ca8009fe1c5db00bbc177c0551f18f
Rachel Xiang <rachelxiangg@gmail.com>
2014-05-10 22:35:37 -0400

    added backbone and dependencies

--- web/js/app.js
+++ web/js/app.js
@@ -1,3 +1,12 @@
 /**
  * Created by rachel on 5/10/14.
  */
+// app.js
+
+window.App = {
+    models: {},
+    collections: {},
+    mixins: {},
+    views: {},
+    routers:{}
+};
--- web/js/setup.js
+++ web/js/setup.js
@@ -1,3 +1,17 @@
 /**
  * Created by rachel on 5/10/14.
  */
+
+
+// we'll fill this later with real thngs
+$(function() {
+    Backbone.pubSub = _.extend({}, Backbone.Events);
+
+    // Initialize Backbone views.
+    //  err do something
+
+    App.router = new App.Router;
+
+    // Initialize the Backbone router.
+    Backbone.history.start();
+});
